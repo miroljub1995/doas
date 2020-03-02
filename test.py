9e2d4fd62f4cf69aac594991279285423e4a7b29
miroljub <miroljub1995@gmail.com>
2020-03-02 18:49:24 +0100

    ...

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import cv2
 from api.yolo import yolo
+from api.camera_distance.distance import calculate_fl_obstacle , obstacle_distance
 
+#print(calculate_fl_obstacle())
+#exit()
+
+cam=cv2.VideoCapture(0)
+ret,img=cam.read()
 y = yolo.create()
-# img = Image.open("api/yolo/test_img.jpg")
-img = cv2.imread("api/yolo/test_img.jpg")
+img = cv2.resize(img, (416, 416))
 out = y.detect_image_clean(img)
-
-distances
-
-print(out)
+#y1, x1, y2, x2 = out[0][0]
+print(obstacle_distance(img, out[0]))
