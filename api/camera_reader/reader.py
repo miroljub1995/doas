b78fd2c6f5126d149216b164bfc9afb7ad5e3509
miroljub <miroljub1995@gmail.com>
2020-03-05 16:20:38 +0100

    another impl of Camera Reader

--- api/camera_reader/reader.py
+++ api/camera_reader/reader.py
@@ -3,15 +3,15 @@ import threading
 
 class CameraReader (threading.Thread):
     def __init__(self):
-      threading.Thread.__init__(self)
-      self.cap = cv2.VideoCapture(0)
-      self.res = self.cap.read()
-      self.running = True
-      self.start()
+        threading.Thread.__init__(self)
+        self.cap = cv2.VideoCapture(0)
+        self.res = self.cap.read()
+        self.running = True
+        self.start()
 
     def run(self):
-       while self.running:
-           self.res = self.cap.read()
+        while self.running:
+            self.res = self.cap.read()
 
     def read(self):
         return self.res
@@ -29,5 +29,25 @@ def main():
         cv2.waitKey(500)
     reader.release()
     cv2.destroyAllWindows()
+
+
+class CameraReader1 (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.cap = cv2.VideoCapture(0)
+        self.running = True
+        self.start()
+
+    def run(self):
+        while self.running:
+            self.cap.grab()
+
+    def read(self, image=None):
+        return self.cap.retrieve(image=image)
+
+    def release(self):
+        self.running = False
+        self.join()
+        self.cap.release()
 
 # main()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from api.camera_reader.reader import CameraReader
+from api.camera_reader.reader import CameraReader, CameraReader1
+from api.dwa.dwa import DWA
 from api.yolo import yolo
 from api.projective_transformation import transform
 
@@ -8,22 +9,34 @@ import cv2
 
 
 obj_det = yolo.create()
-reader = CameraReader()
-# car initialization
-perspective_size = (20, 40)
+dwa = DWA()
+reader = None
+try:
+    # reader = CameraReader()
+    reader = CameraReader1()
+    # reader = cv2.VideoCapture(0)
 
-while True:
-    start = time()
-    try: # not to brake program if one frame fail
-        ret, img = reader.read()
-        img = cv2.resize(img, (416, 416))
-        objs, scores, labels = obj_det.detect(img)
-        # perspective_objs = transform.get_perspective_coordinates(objs, perspective_size)
-        # create array of obstacles and goal estimation for dwa
-        # get angle from dwa
-        # turn wheels left/right according to angle from dwa
-        end = time()
+    # car initialization here
+    perspective_size = (20, 40)
+    while True:
+        start = time()
+
+        try: # not to brake program if one frame fail
+            ret, img = reader.read()
+            print("Read time: {}".format(time() - start))
+            img = cv2.resize(img, (416, 416))
+            objs, scores, labels = obj_det.detect(img)
+            # perspective_objs = transform.get_perspective_coordinates(objs, perspective_size)
+            # create array of obstacles and goal estimation for dwa
+            # get angle from dwa
+            # turn wheels left/right according to angle from dwa
+            cv2.imshow('img', img)
+            cv2.waitKey(1)
+            end = time()
+        except Exception as e:
+            print("Some error occured: {}".format(e))
+
         print("Time for frame: {}".format(end - start))
-    except Exception as e:
-        # print("Some error occured: {}".format(err))
-        print("Some error occured: {}".format(e))
+finally:
+    if reader != None:
+        reader.release()
